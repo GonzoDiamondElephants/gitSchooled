f7607be76cff643b88a67a906b3554f621804c2d
Sian Culligan <SianCulligan@gmail.com>
2020-08-15 18:15:45 -0700

    subtle changes

--- middleware/model-finder.js
+++ middleware/model-finder.js
@@ -6,20 +6,20 @@ const Model = require('../models/model.js');
 console.log('model finder middler ware');
 const modelFinder = (req, res, next) => {
   switch (req.params.model) {
-    case 'student':
-      req.collectionModel = new Model(studentSchema);
-      console.log('', req.collectionModel);
-      next();
-      break;
-    case 'potions':
-      req.collectionModel = new Model(potionSchema);
-      next();
-      break;
+  case 'student':
+    req.collectionModel = new Model(studentSchema);
+    console.log('', req.collectionModel);
+    next();
+    break;
+  case 'potions':
+    req.collectionModel = new Model(potionSchema);
+    next();
+    break;
 
-    default:
-      res.status(404);
-      res.end();
-      break;
+  default:
+    res.status(404);
+    res.end();
+    break;
   }
   next();
 };
